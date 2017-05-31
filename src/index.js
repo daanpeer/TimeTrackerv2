3aa9010320fdb1452263a66b8fe5c6acefadf6b0
Daan Houweling <daan.gdh@gmail.com>
2017-05-31 22:25:32 +0200

    Update create-react-app scripts

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import registerServiceWorker from './registerServiceworker'
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 )
+
+registerServiceWorker()
